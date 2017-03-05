c248f59f290f7a76e4714aaf416d0906211365df
ChauMing <971334483@qq.com>
2017-03-05 14:05:08 +0800

    fix: build 后静态文件路径不对的问题

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../../../public/home/index.html'),
     assetsRoot: path.resolve(__dirname, '../../../public/home/'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsSubDirectory: '/static',
+    assetsPublicPath: '/home',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
