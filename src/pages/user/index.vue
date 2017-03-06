9818e09e6f77594f9e766fb295489d1471846aad
ChauMing <971334483@qq.com>
2017-03-06 20:59:02 +0800

    fix: user info page margin-bottom

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,9 +1,12 @@
 <style lang="less">
+.user-info-container {
+  padding-bottom: 100px;
+}
 </style>
 
 <template>
   <div>
-    <router-view></router-view>
+    <router-view class="user-info-container"></router-view>
     <navbar v-show="show"/>
   </div>
 </template>
