76cf51369de4decefaab125ff2f0dd3e96fa8794
ChauMing <971334483@qq.com>
2017-03-10 21:33:40 +0800

    feat: 下拉获取新数据

--- src/pages/home/post.vue
+++ src/pages/home/post.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="problem_container-mixin">
+  <div>
     <router-link v-for="problem in problemList" :to="`/detail/${problem.id}`">
       <problem-item
         v-on:favorite="favorite"
@@ -15,44 +15,39 @@
 
   export default  {
     components: {
-      problemItem
+      problemItem,
     },
     data(){
       return {
         type: '',
-        id: 0,
+        id: '0',
         problemList: [],
-        categorgs: {}
+        categorgs: {},
+        fetchURL: '',
+        page: 1,
+        busy: false
       }
     },
     methods: {
-      fetchProblemList(type, id) {
+      loadMore() {
 
-        this.id = id || ~~this.$route.params['id']
-        this.type = type || this.$route.params['type']
+      },
+      fetchProblemList() {
 
-        console.log(this.id , this.type)
-        // 0 获取所有数据
-        if(this.id === 0) {
-          return this.$http.get(`/q/${this.type}`)
-            .then((res) => {
-              this.problemList =  res.body.map((item, index) => {
-                item.index = index
-                return item
-              })
-            })
-            .catch(console.error)
+        if(this.id === '0') {
+          this.fetchURL = `/q/${this.type}?page=${this.page}`
+        } else {
+          this.fetchURL = `/q/${this.type}?category_id=${this.id}&page=${this.page}`
         }
 
         // 获取某个分类下的最新或者最热消息
-        this.$http.get(`/q/${this.type}?category_id=${this.id}`)
+        return this.$http.get(this.fetchURL)
           .then((res) => {
-            this.problemList = res.body.map((item, index) => {
-              item.index = index
+            return res.body.map((item, index) => {
+              item.index = index + (this.page * 10 - 10)
               return item
             })
           })
-          .catch(console.error)
       },
       favorite(index, id) {
         this.$http.get(`/favorite/${id}`)
@@ -69,16 +64,45 @@
               this.problemList[index].isFavorite = true
             }
           })
+      },
+      scroll(event) {
+        let viewportHeight = window.innerHeight
+        let scrollY = window.pageYOffset
+        let documentHeight = document.documentElement.offsetHeight
+        if(viewportHeight + scrollY === documentHeight && this.busy === false) {
+          console.log('fetch data')
+          this.busy = true
+          this.page++
+          this.fetchProblemList()
+            .then((problems) => {
+              this.problemList = this.problemList.concat(problems)
+              console.log(this.problemList)
+              this.busy = false
+            })
+        }
       }
     },
     created() {
+      this.type = this.$route.params.type
+      this.id = this.$route.params.id
       this.fetchProblemList()
+        .then((problems) => {
+          this.problemList = problems
+        })
+
+      window.addEventListener('scroll', this.scroll)
     },
     beforeRouteUpdate(to, from , next) {
-      let id = ~~to.params['id']
-      let type = to.params['type']
-      this.fetchProblemList(type, id)
+      this.id = to.params.id
+      this.type = to.params.type
+      this.fetchProblemList()
+        .then((problems) => {
+          this.problemList = problems
+        })
       next(true)
+    },
+    beforeRouteLeave() {
+      window.removeEventListener('scroll', this.scroll)
     }
   }
 </script>
